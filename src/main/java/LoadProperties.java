eb215aa350dab33b17a16b245fec5f8ae0ec23ae
Mike Doyle <mike.doyle@thomsonreuters.com>
2016-05-19 13:31:30 -0500

    Adding new LoadProperties

---
+++ src/main/java/LoadProperties.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.Properties;
+
+public class LoadProperties {
+
+    private static LoadProperties instance = null;
+    private static Properties prop = new Properties();
+
+    public static LoadProperties getInstance() {
+        if (instance == null) {
+            instance = new LoadProperties();
+        }
+
+        return instance;
+    }
+
+    private LoadProperties() {
+        InputStream input = null;
+
+        try {
+
+            if (new File("src/main/config/local.properties").exists()) {
+                input = new FileInputStream("resources/properties/local.properties");
+                System.out.println("Using internal properties file");
+            } else if (new File("local.properties").exists()) {
+                input = new FileInputStream("local.properties");
+                System.out.println("Using external properties file");
+            } else {
+                throw new FileNotFoundException();
+            }
+
+
+            prop.load(input);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public Properties getProperties(){
+        return prop;
+    }
+
+}
